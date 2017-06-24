5e4e08d81eb0469ff0e48e2c28d6d31231b77042
haven <haven@actio.com.au>
2017-06-24 19:07:43 +1000

    starting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit,} from '@angular/core';
+import { hitAreas } from 'ngvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  public pixelHitArea = hitAreas.PixelHitArea;
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+
+  onClickHandler(event) {
+    console.log(event);
+  }
+
+  onNgvasInit() {
+    console.log('onNgvasInit');
+  }
+
 }
